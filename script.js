6223c6f84c33028ddf0938bb2b957dd9476392d3
Sumit Bhardwaj <95293951+smtbhd32@users.noreply.github.com>
2022-01-30 19:25:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-function getTime() {
+function gotTime() {
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   var today = new Date();
+  var d = today.getDate();
+  var mo = today.getMonth();
+  var y = today.getFullYear();
   var h = today.getHours();
   var m = today.getMinutes();
   if (m < 10) {
     m = "0" + m;
   }
-  document.getElementById("myclock").innerHTML = h + ":" + m;
+  var day = months[mo] + " " + d + ", " + y + " ";
+  document.getElementById("myclock").innerHTML = day + h + ":" + m;
 }
 window.onload = function () {
-  getTime();
-  setInterval(getTime, 10000);
+  gotTime();
+  setInterval(gotTime, 10000);
 };
